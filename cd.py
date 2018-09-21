8146b1d10f47907ede4705dc31a3ba5cff8913f1
UserUnavailable <klebersilvanaruto6@gmail.com>
2018-09-21 12:00:33 -0300

    shell

--- cd.py
+++ cd.py
@@ -1,7 +1,7 @@
 ## Python3.5
 ## CD
 
-from os import chdir, environ
+from os import chdir, environ, getcwd
 
 def _cd(param):
           if len(param) == 1 or param[1] == "~": chdir(environ["HOME"])
@@ -10,3 +10,5 @@ def _cd(param):
                     if param[1][0] != "-":
                               try: chdir(_path)
                               except FileNotFoundError: print("cd: error: no such file or directory:", _path)
+                    elif param[1][1:] == "": print(getcwd())
+                    else: print("cd: warning: function not implemented:", param[1])
