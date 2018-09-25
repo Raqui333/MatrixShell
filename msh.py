be2df6998bdcd53b00586f4672d965fbbf71d055
UserUnavailable <klebersilvanaruto6@gmail.com>
2018-09-25 00:32:33 -0300

    features added: Completion

--- msh.py
+++ msh.py
@@ -2,16 +2,28 @@
 ## Python3.5
 ## Matrix Shell
 
-version = "1.2"
+version = "1.3"
 
 from os import *
+from readline import *
+from readline import parse_and_bind as bind
 from msh_builtins import *
 from msh_builtins import _msh_exec
 from socket import gethostname
-from readline import set_history_length, write_history_file, read_history_file
 
-## Default Exit Status
+## Initial Exit Status
 status = 0
+
+## Binds
+bind("set colored-stats on")
+bind("set match-hidden-files off")
+bind("set completion-query-items -1")
+bind("set completion-prefix-display-length 0")
+bind("TAB: complete")
+
+## Completer
+set_completer_delims(" ")
+set_completer(msh_completer)
 
 ## History File
 if path.isfile(histfile) == False: write_history_file(histfile)
@@ -25,13 +37,16 @@ while True:
           currentDirectory = getcwd()
           currentDirectory = currentDirectory.replace("/home/{}".format(user), "~")
 
+          ## DO NOT remove the NEWLINE
           prompt = "\033[1;32m{}\033[1;37m@\033[1;32m{} \033[1;37m{}\033[00m\n$ ".format(user, host, currentDirectory)
           if status != 0: prompt = "\033[1;31m{} {}".format(status, prompt)
 
           try:
+                    ## Prompt
                     commandLine = input(prompt)
                     param = commandLine.split()
 
+                    ## Shell Special Characters
                     elem = 0
                     while elem < len(param):
                               if param[elem][0] == "$":
@@ -42,8 +57,14 @@ while True:
                                                   except KeyError:
                                                             param.remove(param[elem])
                                                             elem = 0
+                              elif param[elem] == "~": param[elem] = environ["HOME"]
+                              elif param[elem][-1] == "\\" and param[elem][:-1] != "":
+                                        param[elem] = " ".join([param[elem][:-1], param.pop(elem + 1)])
+                                        elem = 0
+                              
                               elem += 1
 
+                    ## Run Commands
                     if param != []:
                               for progs in programlist:
                                         if param[0] == progs:
